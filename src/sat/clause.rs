bb71a50331c1162e7fbfb81006484d2d5de10b52
vrindisbacher <vrindisbacher77@gmail.com>
2024-11-14 07:48:19 -0800

    Move maps to solver rather than dirtying clauses and formulas with them

--- src/sat/clause.rs
+++ src/sat/clause.rs
@@ -2,92 +2,15 @@ use std::clone::Clone;
 use std::fmt::Debug;
 use std::hash::Hash;
 
-use super::assignment::Assignments;
-use super::formula::CnfFormula;
 use crate::var::Lit;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct CnfClause<T: PartialEq + Eq + Hash + Debug + Clone> {
     pub vars: Vec<Lit<T>>,
-    pub watchlist: Vec<usize>,
 }
 
 impl<T: PartialEq + Eq + Hash + Debug + Clone> CnfClause<T> {
     pub fn new(vars: Vec<Lit<T>>) -> Self {
-        let watchlist;
-        if vars.len() >= 2 {
-            watchlist = vec![0, 1];
-        } else {
-            watchlist = vec![0]
-        }
-        Self { vars, watchlist }
-    }
-
-    pub fn is_watching_at_least_one_true(&self, assignment: &Assignments<T>) -> bool {
-        for idx in self.watchlist.iter() {
-            if let Some(assn) = assignment.get_assignment(&self.vars[*idx]) {
-                if assn {
-                    return true;
-                }
-            }
-        }
-        false
-    }
-
-    pub fn resolve_watch(
-        &self,
-        formula: &mut CnfFormula<T>,
-        lit_to_change: &Lit<T>,
-        assignment: &mut Assignments<T>,
-    ) -> Result<(), ()> {
-        let to_change_idxs = self
-            .watchlist
-            .iter()
-            .enumerate()
-            .find(|(_, idx)| self.vars[**idx] == *lit_to_change);
-        if let Some((to_change_wl_idx, idx_to_change)) = to_change_idxs {
-            // now determine is we're looking at 2 vars or 1
-            let mut other_idx = None;
-            if self.watchlist.len() == 2 {
-                other_idx = Some(self.watchlist[1 - to_change_wl_idx]);
-            }
-            let mut new_idx = -1;
-            for (idx, var) in self.vars.iter().enumerate() {
-                if idx == *idx_to_change
-                    || other_idx.is_some_and(|other_idx: usize| other_idx == idx)
-                {
-                    continue;
-                }
-                if let Some(assn) = assignment.get_assignment(var) {
-                    if assn {
-                        new_idx = idx as i32;
-                    }
-                } else {
-                    new_idx = idx as i32;
-                }
-            }
-            if new_idx == -1 {
-                if let Some(other_idx) = other_idx {
-                    let other_watched = &self.vars[other_idx];
-                    if let Some(assn) = assignment.get_assignment(other_watched) {
-                        if !assn {
-                            return Err(());
-                        }
-                    }
-                    let assn_val = !other_watched.is_negated();
-                    assignment.assign(other_watched.get_name().clone(), assn_val);
-                } else {
-                    return Err(());
-                }
-            } else {
-                // updates done to main formula clauses - at this point these are cloned
-                formula.update_clause_watchlist(self, to_change_wl_idx, new_idx as usize);
-                formula.remove_watching_clause_for_var(lit_to_change, self);
-                formula.add_watching_clause_for_var(&self.vars[new_idx as usize], self);
-            }
-            Ok(())
-        } else {
-            Err(())
-        }
+        Self { vars }
     }
 }
--- src/sat/formula.rs
+++ src/sat/formula.rs
@@ -1,16 +1,12 @@
 use super::clause::CnfClause;
-use crate::var::{IntoCnf, Lit};
-
+use crate::var::IntoCnf;
 use std::clone::Clone;
-use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 
 #[derive(Debug, Clone)]
 pub(crate) struct CnfFormula<T: PartialEq + Eq + Hash + Debug + Clone> {
     pub clauses: Vec<CnfClause<T>>,
-    // var name and negation to index of clauses in formula...
-    pub watched_by_map: HashMap<(T, bool), Vec<usize>>,
 }
 
 impl<T: PartialEq + Eq + Hash + Debug + Clone> IntoCnf<T> for CnfFormula<T> {
@@ -21,79 +17,6 @@ impl<T: PartialEq + Eq + Hash + Debug + Clone> IntoCnf<T> for CnfFormula<T> {
 
 impl<T: PartialEq + Eq + Hash + Debug + Clone> CnfFormula<T> {
     pub fn new(clauses: Vec<CnfClause<T>>) -> Self {
-        // do some preprocessing for clauses
-        let watched_by_map = Self::create_watchlist_map(&clauses);
-        Self {
-            clauses,
-            watched_by_map,
-        }
-    }
-
-    fn create_watchlist_map(clauses: &Vec<CnfClause<T>>) -> HashMap<(T, bool), Vec<usize>> {
-        let mut map = HashMap::new();
-        for (idx, clause) in clauses.iter().enumerate() {
-            for watch_idx in clause.watchlist.iter() {
-                let var = &clause.vars[*watch_idx];
-                let var_name = var.get_name().clone();
-                let var_negated = var.is_negated();
-                map.entry((var_name, var_negated))
-                    .and_modify(|e: &mut Vec<usize>| {
-                        if !e.contains(&idx) {
-                            e.push(idx)
-                        }
-                    })
-                    .or_insert_with(|| vec![idx]);
-            }
-        }
-        map
-    }
-
-    pub fn remove_watching_clause_for_var(&mut self, lit: &Lit<T>, clause: &CnfClause<T>) {
-        self.watched_by_map
-            .entry((lit.get_name().clone(), lit.is_negated()))
-            .and_modify(|e| {
-                if let Some(idx) = e.iter().position(|i| self.clauses[*i].vars == *clause.vars) {
-                    e.remove(idx);
-                }
-            });
-    }
-
-    pub fn add_watching_clause_for_var(&mut self, lit: &Lit<T>, clause: &CnfClause<T>) {
-        if let Some(clause_idx) = self.clauses.iter().position(|c| c.vars == clause.vars) {
-            self.watched_by_map
-                .entry((lit.get_name().clone(), lit.is_negated()))
-                .and_modify(|e| {
-                    if !e.contains(&clause_idx) {
-                        e.push(clause_idx);
-                    }
-                });
-        }
-    }
-
-    pub fn get_watching_clauses_for_var(&self, lit: &Lit<T>) -> Vec<CnfClause<T>> {
-        let mut vec = vec![];
-        if let Some(clause_idxs) = self
-            .watched_by_map
-            .get(&(lit.get_name().clone(), lit.is_negated()))
-        {
-            for idx in clause_idxs {
-                let clause = self.clauses[*idx].clone();
-                vec.push(clause);
-            }
-        }
-        vec
-    }
-
-    pub fn update_clause_watchlist(
-        &mut self,
-        clause: &CnfClause<T>,
-        to_change_wl_idx: usize,
-        new_idx: usize,
-    ) {
-        if let Some(clause_to_update_idx) =
-            self.clauses.iter_mut().position(|c| c.vars == clause.vars)
-        {
-            self.clauses[clause_to_update_idx].watchlist[to_change_wl_idx] = new_idx;
-        }
+        Self { clauses }
     }
 }
