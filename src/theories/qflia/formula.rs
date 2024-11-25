8c809d786c6f2dc5b139ae1dcd252ae99ea8c0cb
vrindisbacher <vrindisbacher77@gmail.com>
2024-11-25 08:50:06 -0800

    restrict things at the type level

--- src/theories/qflia/formula.rs
+++ src/theories/qflia/formula.rs
@@ -2,113 +2,104 @@ use crate::theories::formula::SMTFormula;
 use std::fmt::Debug;
 use std::hash::Hash;
 
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-pub(crate) enum QFLIAUnOp {
-    Neg,
-}
-
-#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
-pub(crate) enum QFLIABinOp {
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub(crate) enum BinaryOp {
     Add,
     Mul,
+    Sub,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub(crate) enum Expr<T: Debug + Hash + PartialEq + Eq> {
+    Atom(Int<T>),
+    BinExpr(Box<Expr<T>>, Box<Expr<T>>, BinaryOp),
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+pub(crate) enum CompOp {
     Gte,
     Lte,
 }
 
-#[allow(private_interfaces)]
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
-pub enum QFLIAFormula<T: Debug + Hash + PartialEq + Eq> {
-    Atom(Int<T>),
-    UnaryExpr(Box<QFLIAFormula<T>>, QFLIAUnOp),
-    BinExpr(Box<QFLIAFormula<T>>, Box<QFLIAFormula<T>>, QFLIABinOp),
+pub(crate) struct QFLIAFormula<T: Debug + Hash + PartialEq + Eq> {
+    pub lhs: Expr<T>,
+    pub rhs: Expr<T>,
+    pub op: CompOp,
 }
 
-pub trait IntoQFLIAFormula<T: Debug + Hash + PartialEq + Eq> {
-    fn into_qflia(self) -> QFLIAFormula<T>;
+pub trait IntoExpr<T: Debug + Hash + PartialEq + Eq> {
+    fn into_expr(self) -> Expr<T>;
 }
 
-pub trait QFLIAOp<T: Debug + Clone + Hash + PartialEq + Eq>: IntoQFLIAFormula<T> + Sized {
-    //
-    // Note: We cannot construct invalid formulas because all
-    // ops that return boolean expressions (i.e. not arithmetic)
-    // return SMTFormulas, not QFLIA formulas
-    //
-    // Therefore, the invariant that arguments to functions like
-    // add or sub are only arithmetic ops is automatically upheld
-    // by the type system
-    //
-
-    fn add(self, rhs: impl IntoQFLIAFormula<T>) -> QFLIAFormula<T> {
-        QFLIAFormula::BinExpr(
-            Box::new(self.into_qflia()),
-            Box::new(rhs.into_qflia()),
-            QFLIABinOp::Add,
+pub trait QFLIAOp<T: Debug + Clone + Hash + PartialEq + Eq>: IntoExpr<T> + Sized {
+    fn add(self, rhs: impl IntoExpr<T>) -> Expr<T> {
+        Expr::BinExpr(
+            Box::new(self.into_expr()),
+            Box::new(rhs.into_expr()),
+            BinaryOp::Add,
         )
     }
 
-    fn sub(self, rhs: impl IntoQFLIAFormula<T>) -> QFLIAFormula<T> {
+    fn sub(self, rhs: impl IntoExpr<T>) -> Expr<T> {
         // turns x - y into x + (-y)
-        QFLIAFormula::BinExpr(
-            Box::new(self.into_qflia()),
-            Box::new(rhs.into_qflia().neg()),
-            QFLIABinOp::Add,
+        Expr::BinExpr(
+            Box::new(self.into_expr()),
+            Box::new(rhs.into_expr()),
+            BinaryOp::Sub,
         )
     }
 
-    fn gte(self, rhs: impl IntoQFLIAFormula<T>) -> SMTFormula<QFLIAFormula<T>> {
+    fn gte(self, rhs: impl IntoExpr<T>) -> SMTFormula<QFLIAFormula<T>> {
         // normalized so that we are always comparing to 0
-        SMTFormula::Atom(QFLIAFormula::BinExpr(
-            Box::new(self.into_qflia().sub(rhs.into_qflia())),
-            Box::new(Int::from_const(0).into_qflia()),
-            QFLIABinOp::Gte,
-        ))
+        SMTFormula::Atom(QFLIAFormula {
+            lhs: self.into_expr().sub(rhs.into_expr()),
+            rhs: Int::from_const(0).into_expr(),
+            op: CompOp::Gte,
+        })
     }
 
-    fn lte(self, rhs: impl IntoQFLIAFormula<T>) -> SMTFormula<QFLIAFormula<T>> {
+    fn lte(self, rhs: impl IntoExpr<T>) -> SMTFormula<QFLIAFormula<T>> {
         // normalized so that we are always comparing to 0
-        SMTFormula::Atom(QFLIAFormula::BinExpr(
-            Box::new(self.into_qflia().sub(rhs.into_qflia())),
-            Box::new(Int::from_const(0).into_qflia()),
-            QFLIABinOp::Lte,
-        ))
+        SMTFormula::Atom(QFLIAFormula {
+            lhs: self.into_expr().sub(rhs.into_expr()),
+            rhs: Int::from_const(0).into_expr(),
+            op: CompOp::Lte,
+        })
     }
 
-    fn gt(self, rhs: impl IntoQFLIAFormula<T>) -> SMTFormula<QFLIAFormula<T>> {
+    fn gt(self, rhs: impl IntoExpr<T>) -> SMTFormula<QFLIAFormula<T>> {
         // turns gt into not ( lte ) and makes sure we compare to zero
-        self.into_qflia().lte(rhs.into_qflia()).not()
+        self.into_expr().lte(rhs.into_expr()).not()
     }
 
-    fn lt(self, rhs: impl IntoQFLIAFormula<T>) -> SMTFormula<QFLIAFormula<T>> {
+    fn lt(self, rhs: impl IntoExpr<T>) -> SMTFormula<QFLIAFormula<T>> {
         // turns lt into not ( gte )
-        self.into_qflia().gte(rhs.into_qflia()).not()
+        self.into_expr().gte(rhs.into_expr()).not()
     }
 
-    fn eq(self, rhs: impl IntoQFLIAFormula<T> + Clone) -> SMTFormula<QFLIAFormula<T>> {
+    fn eq(self, rhs: impl IntoExpr<T> + Clone) -> SMTFormula<QFLIAFormula<T>> {
         // turns eq into gte && lte
-        let lhs = self.into_qflia();
-        let rhs = rhs.into_qflia();
+        let lhs = self.into_expr();
+        let rhs = rhs.into_expr();
         lhs.clone().gte(rhs.clone()).and(lhs.lte(rhs))
     }
 
-    fn neq(self, rhs: impl IntoQFLIAFormula<T>) -> SMTFormula<QFLIAFormula<T>> {
+    fn neq(self, rhs: impl IntoExpr<T>) -> SMTFormula<QFLIAFormula<T>> {
         // turns neq into gt && lt
-        let lhs = self.into_qflia();
-        let rhs = rhs.into_qflia();
+        let lhs = self.into_expr();
+        let rhs = rhs.into_expr();
         lhs.clone().gt(rhs.clone()).and(lhs.lt(rhs))
-    }
-
-    fn neg(self) -> QFLIAFormula<T> {
-        QFLIAFormula::UnaryExpr(Box::new(self.into_qflia()), QFLIAUnOp::Neg)
     }
 }
 
-impl<T: Debug + Hash + PartialEq + Eq> IntoQFLIAFormula<T> for QFLIAFormula<T> {
-    fn into_qflia(self) -> QFLIAFormula<T> {
+impl<T: Debug + Clone + Hash + PartialEq + Eq> IntoExpr<T> for Expr<T> {
+    fn into_expr(self) -> Expr<T> {
         self
     }
 }
 
-impl<T: Debug + Clone + Hash + PartialEq + Eq> QFLIAOp<T> for QFLIAFormula<T> {}
+impl<T: Debug + Clone + Hash + PartialEq + Eq> QFLIAOp<T> for Expr<T> {}
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum Int<T: Debug + Hash + PartialEq + Eq> {
@@ -136,18 +127,18 @@ impl<T: Debug + Hash + PartialEq + Eq> Int<T> {
     //
     // Restricting mul to Int::from_const(3).mul(Int::from_var(x).add(Int::from_var(y)))
     // does this.
-    pub fn mul(self, rhs: impl IntoQFLIAFormula<T>) -> QFLIAFormula<T> {
-        QFLIAFormula::BinExpr(
-            Box::new(self.into_qflia()),
-            Box::new(rhs.into_qflia()),
-            QFLIABinOp::Mul,
-        )
-    }
+    // pub fn mul(self, rhs: impl IntoQFLIAFormula<T>) -> QFLIAFormula<T> {
+    //     QFLIAFormula::BinExpr(
+    //         Box::new(self.into_qflia()),
+    //         Box::new(rhs.into_qflia()),
+    //         QFLIABinOp::Mul,
+    //     )
+    // }
 }
 
-impl<T: Debug + Hash + PartialEq + Eq> IntoQFLIAFormula<T> for Int<T> {
-    fn into_qflia(self) -> QFLIAFormula<T> {
-        QFLIAFormula::Atom(self)
+impl<T: Debug + Hash + PartialEq + Eq> IntoExpr<T> for Int<T> {
+    fn into_expr(self) -> Expr<T> {
+        Expr::Atom(self)
     }
 }
 
