1adaef91ecd86b6f4f52c52b977b126cb2c5b04b
vrindisbacher <vrindisbacher77@gmail.com>
2024-11-24 16:38:45 -0800

    rework

--- src/theories/qflia/mod.rs
+++ src/theories/qflia/mod.rs
@@ -33,56 +33,20 @@ impl QFLIASolverResult {
 pub struct QFLIASolver<T: Debug + Hash + PartialEq + Eq> {
     // QFLIA solver has constraints which are QFLIA Formulas
     // possibly joined by conjunctions or disjunctions or other crazy stuff
-    constraints: Vec<QFLIASolverOperand<T>>,
+    goals: Vec<QFLIASolverOperand<T>>,
 }
 
 impl<T: Debug + Hash + PartialEq + Eq> QFLIASolver<T> {
     pub fn new() -> Self {
-        Self {
-            constraints: Vec::new(),
-        }
+        Self { goals: Vec::new() }
     }
 
-    pub fn add_constraint(&mut self, constraint: QFLIASolverOperand<T>) {
-        self.constraints.push(constraint);
+    pub fn assert(&mut self, goal: QFLIASolverOperand<T>) {
+        self.goals.push(goal);
     }
 
-    pub fn assert(&mut self, goal: QFLIASolverOperand<T>) -> QFLIASolverResult {
-        // turn this into a SAT formula now
-        let sat_prop = self.into_sat_prop(&goal);
-        if let SATSolverResult::Sat(_assns) = self.find_sat_assignment(sat_prop) {
-            todo!()
-        } else {
-            QFLIASolverResult::Unsat
-        }
-    }
-
-    fn find_sat_assignment(&self, sat_prop: SATProp<u32>) -> SATSolverResult<u32> {
-        let solver = crate::sat::SATSolver::new(sat_prop.into_cnf());
-        solver.run()
-    }
-
-    fn into_sat_prop(&self, goal: &QFLIASolverOperand<T>) -> SATProp<u32> {
-        let mut smt_to_sat_resolver = SMTtoSatPropResolver::new();
-        let mut curr_formula: SATProp<u32>;
-        // and turn the goal into a prop
-        let goal_prop = smt_to_sat_resolver.expr_to_sat_prop(goal);
-        // if there are no constraints and just a goal
-        // then return that prop on its own
-        if self.constraints.len() == 0 {
-            return goal_prop;
-        }
-        // otherwise replace all constraints with Prop's that are joined
-        // by conjunction
-        curr_formula = smt_to_sat_resolver.expr_to_sat_prop(&self.constraints[0]);
-        // skip the first since we've looked at it
-        for constraint in self.constraints.iter().skip(1) {
-            let prop = smt_to_sat_resolver.expr_to_sat_prop(constraint);
-            curr_formula = curr_formula.and(prop);
-        }
-        // and imply the goal from the constraints
-        curr_formula = curr_formula.implies(goal_prop);
-        curr_formula
+    pub fn solve(&self) -> QFLIASolverResult {
+        todo!()
     }
 }
 
